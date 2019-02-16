5e95532f855b4a618ce5ed6a4deb2e32605078df
Benedictiknkeonye@gmail.com <Benedictiknkeonye@gmail.com>
2019-02-16 21:45:16 +0100

    created menu model

---
+++ API/utils/menuDummyData.js
@@ -0,0 +1,55 @@
+export default {
+    menus: [
+        {
+            date: "20th February, 2019",
+            meals:  [
+                        {
+                            id: 1,
+                            name: 'Coconut Rice',
+                            size: 'Large',
+                            price: "600"
+                        },
+                        {
+                            id: 2,
+                            name: 'Fried Rice',
+                            size: 'Medium',
+                            price: "700"
+                        },
+                        {
+                            id: 3,
+                            name: 'Ewa agoyin',
+                            size: 'Large',
+                            price: "350"
+                        },
+                    ]
+        },
+        
+    
+
+    {
+        date: "21st February, 2019",
+        meals:  [
+                    {
+                        id: 1,
+                        name: 'Coconut Rice',
+                        size: 'Large',
+                        price: "600"
+                    },
+                    {
+                        id: 2,
+                        name: 'Fried Rice',
+                        size: 'Medium',
+                        price: "700"
+                    },
+                    {
+                        id: 3,
+                        name: 'Ewa agoyin',
+                        size: 'Large',
+                        price: "350"
+                    },
+                ]
+            }
+    
+    ]
+    
+}
