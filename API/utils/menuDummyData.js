b935b761f27025c3f00620b2d1224fb299f12b35
Benedictiknkeonye@gmail.com <Benedictiknkeonye@gmail.com>
2019-02-16 22:38:47 +0100

    Created meal service

--- API/utils/menuDummyData.js
+++ API/utils/menuDummyData.js
@@ -1,7 +1,7 @@
 export default {
-    menus: [
-        {
-            date: "20th February, 2019",
+    menu: [
+        {   
+            date: "16th February, 2019",
             meals:  [
                         {
                             id: 1,
@@ -23,33 +23,5 @@ export default {
                         },
                     ]
         },
-        
-    
-
-    {
-        date: "21st February, 2019",
-        meals:  [
-                    {
-                        id: 1,
-                        name: 'Coconut Rice',
-                        size: 'Large',
-                        price: "600"
-                    },
-                    {
-                        id: 2,
-                        name: 'Fried Rice',
-                        size: 'Medium',
-                        price: "700"
-                    },
-                    {
-                        id: 3,
-                        name: 'Ewa agoyin',
-                        size: 'Large',
-                        price: "350"
-                    },
-                ]
-            }
-    
     ]
-    
 }
