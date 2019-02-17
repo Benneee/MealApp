d0f3f7a57bda71b49cead3b21c2c08f250ed2158
Benedictiknkeonye@gmail.com <Benedictiknkeonye@gmail.com>
2019-02-17 02:16:20 +0100

    created order  models

---
+++ API/utils/orderDummyData.js
@@ -0,0 +1,48 @@
+export default {
+    orders: [
+        {
+            id: 1,
+            meal_order: [
+                {
+                    id: 1,
+                    name: 'Coconut Rice',
+                    size: 'Large',
+                    price: "600"
+                },
+                {
+                    id: 2,
+                    name: 'Fried Rice',
+                    size: 'Medium',
+                    price: "700"
+                },
+            ] ,
+            customer_name: "Tola Titi",
+            customer_id: 1, 
+            address: "5 address street, place",
+            phone_number: "08096785678",
+            purchase_total: "1300"
+        },
+        {
+            id: 2,
+            meal_order: [
+                {
+                    id: 1,
+                    name: 'Beans and Dodo',
+                    size: 'Large',
+                    price: "300"
+                },
+                {
+                    id: 2,
+                    name: 'Amala and Ewedu',
+                    size: 'Medium',
+                    price: "500"
+                },
+            ] ,
+            customer_name: "Titi Tolu",
+            customer_id: 1, 
+            address: "5 address street, place",
+            phone_number: "08096785678",
+            purchase_total: "1300"
+        }
+    ],
+};
