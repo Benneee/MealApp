789b42c8cc4e6ce340ddd2d8097c206dae0a3208
Benedictiknkeonye@gmail.com <Benedictiknkeonye@gmail.com>
2019-02-21 02:20:47 +0100

    installed dependencies for test environment setup

--- API/src/tests/index.test.js
+++ API/src/tests/index.test.js
@@ -1,9 +1,13 @@
+// const expect = require('expect');
+
 const index = require('../index');
 
 it('should add two numbers', () => {
     var res = index.add(20, 20);
 
-    if(res !== 40) {
-        throw new Error(`Expected 40, but got ${res}.`)
-    }
+    // if(res !== 40) {
+    //     throw new Error(`Expected 40, but got ${res}.`)
+    // }
+
+    expect(res).toBe(40);
 });
