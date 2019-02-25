acd03224c13c46beac1107a6c001eb2896ad262b
Benedictiknkeonye@gmail.com <Benedictiknkeonye@gmail.com>
2019-02-25 09:00:06 +0100

    Updated index.js file

--- tests/index.test.js
+++ tests/index.test.js
@@ -4,16 +4,11 @@
 
 
 describe('API Test', () => {
-    // it('should return Holla Benneee! as response', (done) => {
-    //     request(app)
-    //         .get('/')
-    //         .expect(200)
-    //         .expect('Holla Benneee!')
-    //         .end(done);
-    // });
-
-    it('should return null', (done) => {
-        
-    })
-    done()
+    it('should return Holla Benneee! as response', (done) => {
+        request(app)
+            .get('/')
+            .expect(200)
+            .expect('Holla Benneee!')
+            .end(done);
+    });
 });
