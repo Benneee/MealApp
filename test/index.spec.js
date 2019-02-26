4a89d71f4ef2abf784baef152bf728454524636d
Benedictiknkeonye@gmail.com <Benedictiknkeonye@gmail.com>
2019-02-26 06:54:47 +0100

    Updated test file

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,12 +1,11 @@
+const server = require('../index');
 const request = require('supertest')
-const app = require('../api/src/index')
 
-describe('API Test', () => {
-    it('should return Welcome To Book-A-Meal! as response', (done) => {
-        request(app)
-            .get('/')
-            .expect(200)
-            .expect('Welcome To Book-A-Meal!')
-            .end(done);
-    });
+describe('test', () => {
+  it('should return a string', (done) => {
+      request(server)
+        .expect('ci with travis')
+        .expect(200)
+        .end(done);
+  });
 });
