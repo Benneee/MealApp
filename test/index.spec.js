a05f70422043078ba1ad05378b7c1bb00684c318
Benedictiknkeonye@gmail.com <Benedictiknkeonye@gmail.com>
2019-02-26 07:03:58 +0100

    Updated file structure

--- test/index.spec.js
+++
@@ -1,9 +0,0 @@
-const expect = require('expect');
-const request = require('supertest')
-
-describe('test', () => {
-  it('should return a string', (done) => {
-    expect('ci with travis')
-        .end(done);
-  });
-});
